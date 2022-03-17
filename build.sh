857e1807c1f8388e7b9c679b8d5b88ab8b42d285
Sneha Kanekar1 <sneha.kanekar1@ibm.com>
2022-03-17 11:43:49 +0530

    build terraform v0.13.0

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ git clone https://github.com/hashicorp/terraform.git
 cd terraform
 TF_LATEST=$(curl -s https://api.github.com/repos/hashicorp/terraform/releases/latest | grep tag_name | cut -d '"' -f4)
 echo $1
-export TAG=${1:TF_LATEST}
+export TAG="${1:TF_LATEST}"
 git checkout tags/$TAG
 
 env GOOS=linux GOARCH=ppc64le go install .
