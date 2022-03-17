27dbe6a4ea6db7cb152d03caeb23b596940c112a
Sneha Kanekar1 <sneha.kanekar1@ibm.com>
2022-03-17 10:23:28 +0530

    build terraform v0.13.0

--- build.sh
+++ build.sh
@@ -7,7 +7,8 @@ source ./go.sh install 1.17.8
 cd ..
 git clone https://github.com/hashicorp/terraform.git
 cd terraform
-export TAG=$1
+TF_LATEST=$(curl -s https://api.github.com/repos/hashicorp/terraform/releases/latest | grep tag_name | cut -d '"' -f4)
+export TAG=${1:TF_LATEST}
 git checkout tags/$TAG
 
 env GOOS=linux GOARCH=ppc64le go install .
