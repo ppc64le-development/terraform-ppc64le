6d4ee7d3c82ef33f3720ebfccc1d43d80418422d
Sneha Kanekar1 <sneha.kanekar1@ibm.com>
2022-03-17 15:43:41 +0530

    add cron job to detect new release

--- build.sh
+++ build.sh
@@ -7,14 +7,8 @@ source ./go.sh install 1.17.8
 cd ..
 git clone https://github.com/hashicorp/terraform.git
 cd terraform
-TF_LATEST=$(curl -s https://api.github.com/repos/hashicorp/terraform/releases/latest | grep tag_name | cut -d '"' -f4)
-if [ $# -eq 0 ]
-  then
-    export TAG=$TF_LATEST
-else
-    export TAG=$1
-fi
-
+TAG=$(curl -s https://api.github.com/repos/hashicorp/terraform/releases/latest | grep tag_name | cut -d '"' -f4)
 git checkout tags/$TAG
 
 env GOOS=linux GOARCH=ppc64le go install .
+
